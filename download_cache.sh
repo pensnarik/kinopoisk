5f9fe4dd477f72bc5c3d4cbd99e3006ce29d81f2
Андрей Жиденков <pensnarik@gmail.com>
2016-12-26 19:09:39 +0300

    Скрипт для обновления кеша

---
+++ download_cache.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+scp -r unica1.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unica2.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unica3.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unica4.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unica5.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unica6.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unica7.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r crm.smartsales.su:~/kinopoisk/cache/* ./cache/
+scp -r new.smartsales.su:~/kinopoisk/cache/* ./cache/
+scp -r store.parselab.ru:~/kinopoisk/cache/* ./cache/
+scp -r unicadb.parselab.ru:~/kinopoisk/cache/* ./cache/
