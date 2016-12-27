f9bcd088a5c52f3e093364706144c95e3988c0b3
Андрей Жиденков <pensnarik@gmail.com>
2016-12-27 15:55:05 +0300

    Поменял scp на rsync

--- download_cache.sh
+++ download_cache.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-scp -r unica1.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unica2.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unica3.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unica4.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unica5.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unica6.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unica7.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r crm.smartsales.su:~/kinopoisk/cache/* ./cache/
-scp -r new.smartsales.su:~/kinopoisk/cache/* ./cache/
-scp -r store.parselab.ru:~/kinopoisk/cache/* ./cache/
-scp -r unicadb.parselab.ru:~/kinopoisk/cache/* ./cache/
+rsync -avz --compress-level=9 --progress unica1:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica2:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica3:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica4:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica5:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica6:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica7:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress crm.smartsales.su:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress new.smartsales.su:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress store.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress mutex@unicadb.parselab.ru:~/kinopoisk/cache .
