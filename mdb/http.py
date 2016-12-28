be109b3af8733e43ba121280e628efb5baa3e551
Андрей Жиденков <pensnarik@gmail.com>
2016-12-28 14:26:53 +0300

    Возможность отправки POST запросов через http.py, добавил salt для определения хеша в кеше

--- mdb/http.py
+++ mdb/http.py
@@ -26,12 +26,13 @@ class Downloader():
     Реализует интерфейс для получения страниц сайта напрямую или через кеш
     """
     @staticmethod
-    def get(url):
-        if Downloader.is_url_in_cache(url):
-            logger.info('Reading %s from cache...' % url)
-            return Downloader.get_from_cache(url)
+    def get(url, method='GET', salt=None, data=None):
+        display_url = url if salt is None else '%s:%s' % (url, salt,)
+        if Downloader.is_url_in_cache(url, salt):
+            logger.info('Reading %s from cache...' % display_url)
+            return Downloader.get_from_cache(url, salt)
         else:
-            logger.info('Downloading %s' % url)
+            logger.info('Downloading %s' % display_url)
             tries_left = 100
             response = None
 
@@ -40,7 +41,15 @@ class Downloader():
                     sleep_time = randint(1, 20)
                     logger.info('Sleeping %s, tries left: %s' % (sleep_time, tries_left))
                     time.sleep(sleep_time)
-                    response = requests.get(url, timeout=5, headers=mdb.helpers.headers)
+
+                    if method == 'GET':
+                        response = requests.get(url, timeout=5, headers=mdb.helpers.headers)
+                    elif method == 'POST':
+                        response = requests.post(url, timeout=5, headers=mdb.helpers.headers,
+                                                 data=data)
+                    else:
+                        raise Exception('Unknown method: %s' % method)
+
                     if 'captchaimg' in response.text:
                         raise GetPageError('Banned')
                     break
@@ -51,44 +60,48 @@ class Downloader():
                     time.sleep(100 * (100 - tries_left))
 
             if response.status_code == 200 and response is not None:
-                Downloader.write_to_cache(url, response.text)
+                Downloader.write_to_cache(url, salt, response.text)
                 return response.text
             else:
                 return None
 
     @staticmethod
-    def get_from_cache(url):
-        logger.info(Downloader.get_cached_filename(url))
+    def get_from_cache(url, salt):
+        logger.info(Downloader.get_cached_filename(url, salt))
         if sys.version_info[0] >= 3:
-            f = open(Downloader.get_cached_filename(url), 'rt')
+            f = open(Downloader.get_cached_filename(url, salt), 'rt')
             data = f.read()
             f.close()
             return data
         else:
-            with codecs.open(Downloader.get_cached_filename(url), 'r', encoding='utf-8') as f:
+            with codecs.open(Downloader.get_cached_filename(url, salt), 'r', encoding='utf-8') as f:
                 data = f.read()
             return data
 
     @staticmethod
-    def write_to_cache(url, data):
+    def write_to_cache(url, salt, data):
         if sys.version_info[0] >= 3:
-            f = open(Downloader.get_cached_filename(url), 'wt')
+            f = open(Downloader.get_cached_filename(url, salt), 'wt')
             f.write(data)
             f.close()
         else:
-            with codecs.open(Downloader.get_cached_filename(url), 'w', encoding='utf-8') as f:
+            with codecs.open(Downloader.get_cached_filename(url, salt), 'w', encoding='utf-8') as f:
                 f.write(data)
-        logger.info('Saved to cache as %s' % Downloader.get_cached_filename(url))
+        logger.info('Saved to cache as %s' % Downloader.get_cached_filename(url, salt))
 
     @staticmethod
     def get_cache_path():
         return os.path.join(os.path.dirname(os.path.abspath(__main__.__file__)), 'cache')
 
     @staticmethod
-    def get_cached_filename(url):
-        return os.path.join(Downloader.get_cache_path(),
-                            hashlib.md5(url.encode('utf8')).hexdigest())
+    def get_cached_filename(url, salt):
+        if salt is not None:
+            key = ('%s:%s' % (url, salt,)).encode('utf8')
+        else:
+            key = url.encode('utf8')
+
+        return os.path.join(Downloader.get_cache_path(), hashlib.md5(key).hexdigest())
 
     @staticmethod
-    def is_url_in_cache(url):
-        return os.path.exists(Downloader.get_cached_filename(url))
+    def is_url_in_cache(url, salt):
+        return os.path.exists(Downloader.get_cached_filename(url, salt))
