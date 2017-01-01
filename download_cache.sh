fd4bb245c02456396dc8e2c3977661c20e9081c7
Andrey Zhidenkov <pensnarik@gmail.com>
2017-01-02 01:41:50 +0300

    Поправил имена хостов

--- download_cache.sh
+++ download_cache.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rsync -avz --compress-level=9 --progress unica1:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica2:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica3:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica4:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica5:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica6:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica7:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica1.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica2.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica3.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica4.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica5.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica6.parselab.ru:~/kinopoisk/cache .
+rsync -avz --compress-level=9 --progress unica7.parselab.ru:~/kinopoisk/cache .
 rsync -avz --compress-level=9 --progress crm.smartsales.su:~/kinopoisk/cache .
 rsync -avz --compress-level=9 --progress new.smartsales.su:~/kinopoisk/cache .
 rsync -avz --compress-level=9 --progress store.parselab.ru:~/kinopoisk/cache .
