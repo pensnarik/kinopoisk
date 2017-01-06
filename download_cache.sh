e43c53d25049bc6b00a245f1fcc1aec8cd2af61a
Андрей Жиденков <pensnarik@gmail.com>
2017-01-06 17:47:13 +0300

    Убрал --compress-level=9

--- download_cache.sh
+++ download_cache.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rsync -avz --compress-level=9 --progress unica1.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica2.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica3.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica4.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica5.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica6.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress unica7.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress crm.smartsales.su:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress new.smartsales.su:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress store.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress mutex@unicadb.parselab.ru:~/kinopoisk/cache .
-rsync -avz --compress-level=9 --progress parser8.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica1.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica2.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica3.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica4.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica5.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica6.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress unica7.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress crm.smartsales.su:~/kinopoisk/cache .
+rsync -avz --progress new.smartsales.su:~/kinopoisk/cache .
+rsync -avz --progress store.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress mutex@unicadb.parselab.ru:~/kinopoisk/cache .
+rsync -avz --progress parser8.parselab.ru:~/kinopoisk/cache .
