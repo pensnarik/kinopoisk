56b7fb4ce56cd548abcb815ae6a9b3b58bf663c9
pensnarik <pensnarik@gmail.com>
2017-09-18 09:56:19 +0300

    Перенёс распаковку файлов в Dockerfile

--- db/install.py
+++ db/install.py
@@ -7,11 +7,6 @@ import sys
 class App():
 
     def run(self):
-        for file in os.listdir('data'):
-            if file.endswith('.bz2'):
-                print("Unpacking data file %s..." % file)
-                sys.stdout.flush()
-                os.system('bunzip2 -f %s' % os.path.join('data', file))
         print("Executing install script on database '%s'" % sys.argv[1])
         os.system('psql -f install.sql "%s" -v database=mdb' % sys.argv[1])
 
